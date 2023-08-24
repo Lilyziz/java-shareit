7583568b874d1f9084500b08392858d9b103d995
V|ka <qianna@yandex.ru>
2023-08-24 22:59:03 +0300

    add-docker (ТЗ 16) v1.10

--- gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -35,7 +35,8 @@ public class BaseClient {
         return post(path, userId, null, body);
     }
 
-    protected <T> ResponseEntity<Object> post(String path, Long userId, @Nullable Map<String, Object> parameters, T body) {
+    protected <T> ResponseEntity<Object> post(String path, Long userId, @Nullable Map<String, Object> parameters,
+                                              T body) {
         return makeAndSendRequest(HttpMethod.POST, path, userId, parameters, body);
     }
 
